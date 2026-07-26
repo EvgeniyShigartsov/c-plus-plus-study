379353b9ee21c8a2ae5e4ae3a90b34f3c2d91e52
Yevhenii Shchyhartsov <evgeniy.shigartsov2@gmail.com>
2026-07-26 15:36:15 +0000

    hm-13: add explorer node

--- homeworks/14_robot-dog-underground-world/robot_ws/src/mission_control/include/mission_control/topic_names.hpp
+++ homeworks/14_robot-dog-underground-world/robot_ws/src/mission_control/include/mission_control/topic_names.hpp
@@ -3,6 +3,7 @@
 namespace mission_control {
 
 constexpr auto kLocalScanTopic = "/robot/local_scan";
+constexpr auto kResultTopic = "/robot/result";
 constexpr auto kMoveTopic = "/robot/cmd_move";
 constexpr auto kStudentStatusTopic = "/student/status";
 constexpr auto kEnemyDownTopic = "/payload/enemy_down";

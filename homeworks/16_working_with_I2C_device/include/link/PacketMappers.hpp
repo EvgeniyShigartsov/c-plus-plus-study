203e4e4c981e8238793cca0e29de4beebac23a08
Yevhenii Shchyhartsov <evgeniy.shigartsov2@gmail.com>
2026-08-23 09:26:06 +0000

    chore: drop hm-16 template (no needed for hm-16)

--- homeworks/16_working_with_I2C_device/include/link/PacketMappers.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "third_party/drone_link.h"
-#include "types.hpp"
-
-DroneTelemetry toDroneTelemetry(const dlink::Telemetry& telemetry);
-
-BombParams toBombParams(const dlink::AmmoCfg& ammo);
-
-DroneConfig buildDroneConfig(const dlink::AmmoCfg& ammo, const dlink::DroneCfg& droneConfig, const dlink::Telemetry& telemetry);

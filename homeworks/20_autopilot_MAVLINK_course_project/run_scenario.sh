6501152f8d3b2a6bb99b0cea7356325214b76b8c
Yevhenii Shchyhartsov <evgeniy.shigartsov2@gmail.com>
2026-09-18 17:34:45 +0000

    hm-20: working on scenarios

--- homeworks/20_autopilot_MAVLINK_course_project/run_scenario.sh
+++ homeworks/20_autopilot_MAVLINK_course_project/run_scenario.sh
@@ -46,7 +46,7 @@ PIDS=()
 PIDS+=("$!")
 "$BIN/hm20_autopilot" --config-path "$CONFIG" --ammo-path "$AMMO" --ballistic-table "$BALLISTIC" --sim-output "$REPO_ROOT/simulation.json" --time-scale "$TIME_SCALE" &
 PIDS+=("$!")
-sleep 1
+sleep 0.2
 "$BIN/hm20_operator_sim" --operator-scenario "$OPERATOR_SCENARIO" --config-path "$CONFIG" --ammo-path "$AMMO" --targets "$TARGETS" --time-scale "$TIME_SCALE" &
 PIDS+=("$!")
 
